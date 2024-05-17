6bf33c8012822b0803f75db3d3f3a67ad118a7c0
Jaefleming86 <145634158+Jaefleming86@users.noreply.github.com>
2024-05-17 16:04:08 -0500

    Update Main.tf

--- Main.tf
+++ Main.tf
@@ -1,38 +1,22 @@
+# Define the provider block
 provider "google" {
-    credentials = file("jae-fleming-adfc3e3b9a68.json")
-    project     = "jae-fleming"
-    region      = "europe-west1"
+    credentials = var.credentials_file
+    project     = var.project
+    region      = var.region
 }
 
-resource "google_compute_instance" "europe_instance" {
-    name         = "europe-instance"
-    machine_type = "e2-micro"
-    zone         = "europe-west1-b"
-
-    boot_disk {
-        initialize_params {
-            image = "debian-cloud/debian-10"
-        }
-    }
-
-    network_interface {
-        network = google_compute_network.europe_network.self_link
-        subnetwork = google_compute_subnetwork.europe_subnet.self_link
-    }
-}
-
-# Define the VPC and subnets for each region
+# Define the network and subnets for each region
 
 # Europe region
-resource "google_compute_network" "europe_network" {
-    name                    = "europe-network"
+resource "google_compute_network" "europenetwork" {
+    name                    = "europenetwork"
     auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "europe_subnet" {
     name          = "europe-subnet"
-    ip_cidr_range = "10.0.0.0/24"
-    network       = google_compute_network.europe_network.self_link
+    ip_cidr_range = var.europe_subnet_cidr
+    network       = google_compute_network.europenetwork.self_link
 }
 
 # Americas regions
@@ -54,15 +38,15 @@ resource "google_compute_subnetwork" "americas_subnet_2" {
 }
 
 # Asia Pacific region
-resource "google_compute_network" "asia_network" {
-    name                    = "asia-network"
+resource "google_compute_network" "asianetwork" {
+    name                    = "asianetwork"
     auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "asia_subnet" {
     name          = "asia-subnet"
     ip_cidr_range = "192.168.0.0/24"
-    network       = google_compute_network.asia_network.self_link
+    network       = google_compute_network.asianetwork.self_link
 }
 
 # Define firewall rules for each region
@@ -70,7 +54,7 @@ resource "google_compute_subnetwork" "asia_subnet" {
 # Europe firewall rule
 resource "google_compute_firewall" "europe_firewall" {
     name    = "europe-firewall"
-    network = google_compute_network.europe_network.self_link
+    network = google_compute_network.europenetwork.self_link
 
     allow {
         protocol = "icmp"
@@ -94,50 +78,80 @@ resource "google_compute_firewall" "americas_firewall" {
         ports    = ["80"]
     }
 
-    source_ranges = ["172.16.1.0/24" , "172.16.2.0/24"]
+    source_ranges = var.americas_firewall_source_ranges
 }
 
 # Asia Pacific firewall rule
 resource "google_compute_firewall" "asia_firewall" {
     name    = "asia-firewall"
-    network = google_compute_network.asia_network.self_link
+    network = google_compute_network.asianetwork.self_link
 
     allow {
         protocol = "tcp"
         ports    = ["3389"]
     }
 
-    source_ranges = ["192.168.0.0/24"]
+    source_ranges = ["0.0.0.0/0"]
 }
 
-resource "google_compute_network_peering" "my_peering" {
-    name          = "my-peering"
-    network       = google_compute_network.europe_network.self_link
-    peer_network  = google_compute_network.peer_network.self_link
+# Define network peering between Europe and Asia
+
+resource "google_compute_network_peering" "europe_to_asia" {
+    name          = "europe-to-asia"
+    network       = google_compute_network.europenetwork.self_link
+    peer_network  = google_compute_network.asianetwork.self_link
 }
 
-resource "google_compute_vpn_tunnel" "my_tunnel" {
-    name            = "my-tunnel"
-    peer_ip         = google_compute_vpn_gateway.peer_gateway.ip_address
-    shared_secret   = var.shared_secret
-    target_vpn_gateway = google_compute_vpn_gateway.my_gateway.self_link
-    vpn_gateway_interface = "vpn_gateway_interface"
+# Define VPN tunnels and gateways
+
+resource "google_compute_vpn_gateway" "mygateway1" {
+    name = "mygateway1"
+    network = google_compute_network.europenetwork.self_link
+}
+
+resource "google_compute_vpn_gateway" "mygateway2" {
+    name = "mygateway2"
+    network = google_compute_network.europenetwork.self_link
+}
+
+resource "google_compute_vpn_tunnel" "my_tunnel1" {
+    name                  = "my-tunnel1"
+    peer_ip               = "191.167.177.0"
+    shared_secret         = var.shared_secret
+    target_vpn_gateway    = google_compute_vpn_gateway.mygateway1.self_link
     local_traffic_selector = ["0.0.0.0/0"]
+    depends_on = [google_compute_forwarding_rule.esp_rule, google_compute_forwarding_rule.udp_500_rule]
 }
 
-resource "google_compute_vpn_gateway" "my_gateway" {
-    name = "my-gateway"
-    network = google_compute_network.europe_network.self_link
+resource "google_compute_vpn_gateway" "peergateway2" {
+    name = "peergateway2"
+    network = google_compute_network.asianetwork.self_link
 }
 
-resource "google_compute_vpn_gateway" "peer_gateway" {
-    name = "peer-gateway"
-    network = google_compute_network.peer_network.self_link
+resource "google_compute_vpn_tunnel" "my_tunnel2" {
+    name            = "my-tunnel2"
+    peer_ip         = "191.167.177.0"
+    shared_secret   = var.shared_secret
+    target_vpn_gateway = google_compute_vpn_gateway.mygateway2.self_link
+    local_traffic_selector = ["0.0.0.0/0"]
+    depends_on = [google_compute_forwarding_rule.esp_rule, google_compute_forwarding_rule.udp_500_rule]
 }
 
-resource "google_compute_forwarding_rule" "my_forwarding_rule" {
-    name = "my-forwarding-rule"
-    target = google_compute_target_vpn_gateway.my_gateway.self_link
-    ip_protocol = "rdp"
-    port_range = "3389"
+resource "google_compute_forwarding_rule" "udp_500_rule" {
+    name        = "udp-500-rule"
+    target      = google_compute_vpn_gateway.mygateway1.self_link
+    ip_protocol = "UDP"
+    port_range  = "500-500"
+    ip_address  = google_compute_address.vpn_address.address
+}
+
+resource "google_compute_forwarding_rule" "esp_rule" {
+    name        = "esp-rule"
+    target      = google_compute_vpn_gateway.mygateway2.self_link
+    ip_protocol = "ESP"
+    ip_address  = google_compute_address.vpn_address.address
+}
+resource "google_compute_address" "vpn_address" {
+  name = "vpn-address"
+    region = "europe-west1"
 }
